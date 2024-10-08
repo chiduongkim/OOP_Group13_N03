6bbf56f4f45cb814eef1ae3be3f607b2628feaeb
dahquang16 <23010230@st.phenikaa-uni.edu.vn>
2024-10-08 11:35:45 +0700

    Update InsertProductExample.java

--- Connection/InsertProductExample.java
+++ Connection/InsertProductExample.java
@@ -1,40 +1,29 @@
-package MySqlExample;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class InsertProductExample {
-    public static void main(String[] args) throws ClassNotFoundException {
-        String host = "mysql-e4643ae-group13.i.aivencloud.com";
-        String port = "13698";
-        String databaseName = "defaultdb";
-        String userName = "avnadmin";
-        String password = "AVNS_fm4xvgmqE69OhX8l1GY";
+public class MySqlExample {
+  public static void main(String[] args) throws ClassNotFoundException {
+    String host = "mysql-e4643ae-group13.i.aivencloud.com";
+    String port = "13698";
+    String databaseName = "defaultdb";
+    String userName = "avnadmin";
+    String password = "AVNS_fm4xvgmqE69OhX8l1GY";
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        try (final Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://" + host + ":" + port + "/" + databaseName + "?sslmode=require", 
-                userName, 
-                password);
-             final PreparedStatement preparedStatement = connection.prepareStatement(
-                     "INSERT INTO sanpham (maSP, tenSP, giaSP, soLuong) VALUES (?, ?, ?, ?)")) {
+    Class.forName("com.mysql.cj.jdbc.Driver");
+    try (final Connection connection =
+                DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + databaseName + "?sslmode=require", userName, password);
+         final Statement statement = connection.createStatement();
+         final ResultSet resultSet = statement.executeQuery("SELECT version() AS version")) {
 
-            preparedStatement.setString(1, maSP);
-            preparedStatement.setString(2, tenSP);
-            preparedStatement.setDouble(3, Double.parseDouble(giaSP));
-            preparedStatement.setInt(4, Integer.parseInt(soLuong));
-
-            int rowsInserted = preparedStatement.executeUpdate();
-
-            if (rowsInserted > 0) {
-                System.out.println("A new product was inserted successfully!");
-            }
-        } catch (SQLException e) {
-            System.out.println("Connection or SQL execution failure.");
-            e.printStackTrace();
-        }
+      while (resultSet.next()) {
+        System.out.println("Version: " + resultSet.getString("version"));
+      }
+    } catch (SQLException e) {
+      System.out.println("Connection failure.");
+      e.printStackTrace();
     }
+  }
 }
-
