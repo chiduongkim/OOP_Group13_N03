8458b77478bb0afea4a7e340b0aa82b5890d21fe
dahquang16 <23010230@st.phenikaa-uni.edu.vn>
2024-10-16 16:17:00 +0700

    Create LuuTruSP.java

---
+++ sanpham/LuuTruSP.java
@@ -0,0 +1,49 @@
+package sanpham;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class LuuTruSP {
+    public static void luuThongTinSP(String maSP, String tenSP, String giaSP, String soLuong, String maKH) {
+        String host = "mysql-e4643ae-group13.i.aivencloud.com";
+        String port = "13698";
+        String databaseName = "defaultdb";
+        String userName = "avnadmin";
+        String password = "AVNS_fm4xvgmqE69OhX8l1GY";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://" + host + ":" + port + "/" + databaseName + "?sslmode=require", 
+                    userName, 
+                    password);
+                 PreparedStatement preparedStatement = connection.prepareStatement(
+                         "INSERT INTO sanpham (maSP, tenSP, giaSP, soluong, maKH) VALUES (?, ?, ?, ?, ?)")) {
+
+                preparedStatement.setString(1, maSP);
+                preparedStatement.setString(2, tenSP);
+                preparedStatement.setDouble(3, Double.parseDouble(giaSP));
+                preparedStatement.setInt(4, Integer.parseInt(soLuong));
+                preparedStatement.setString(5, maKH);
+
+                int rowsInserted = preparedStatement.executeUpdate();
+
+                if (rowsInserted > 0) {
+                    System.out.println("Sản phẩm mới đã được thêm thành công!");
+                }
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Kết nối hoặc truy vấn SQL bị lỗi.");
+            e.printStackTrace();
+        }
+    }
+
+	public static void luuThongTinDonHang(String maDonHang, String maKhachHang) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
